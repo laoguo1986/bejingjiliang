c7fb0cf588328a5fc77f52f22a9c0356b8fd3d29
laoguo1986 <naturalwind@yahoo.cn>
2013-03-01 11:35:29 +0800

    初始程序

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bjjl::Application.config.secret_token = 'db460621f5479e0278090fcc6e22ee3c65b9df8c84f503579280b91918454e2d32165649253d3b7aa174ecfcade9e2a3be6a2e395d9c7a52700bd24278d9eb38'
